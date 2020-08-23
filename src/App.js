849566f617f0deafb2ca36af3ad39d99495c8778
JimmyMcBride <mcbride967@gmail.com>
2020-08-23 00:14:40 -0500

    did the thing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import axios from "axios";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "@apollo/client";
 
 function App() {
-  const [posts, setPosts] = React.useState(null);
-  React.useEffect(() => {
-    axios
-      .get(
-        "https://be-best-markdown-editor-stage.herokuapp.com/api/published-files/folder-name/Jimmy McBride",
-        {
-          headers: { token: "84e7d6e8-bb64-4bdc-bbb4-54859cb4ebe0" },
-        }
-      )
-      .then((res) => setPosts(res.data))
-      .catch((err) => console.log(err.message));
-  }, []);
+  const query = gql`
+    query GetPubFiles {
+      getPubFiles {
+        id
+        title
+        slug
+        body
+        publishedOn
+        updatedAt
+      }
+    }
+  `;
+  const { data } = useQuery(query);
+  console.log("data", data);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {posts?.map((post) => (
+        {/* {posts?.map((post) => (
           <div>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
           </div>
-        ))}
+        ))} */}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {
+  ApolloProvider,
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
+import * as serviceWorker from "./serviceWorker";
+
+const httpLink = new HttpLink({
+  uri: "https://be-best-markdown-editor-stage.herokuapp.com/cms",
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      token: "84e7d6e8-bb64-4bdc-bbb4-54859cb4ebe0",
+    },
+  });
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
